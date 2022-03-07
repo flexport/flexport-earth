9fd25b991639ed6fb7fe5bee1d4050fccadf3b5e
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-03-06 22:47:35 -0800

    earth-8: Redo the privisioning and deprovisioning scripts to use the "az" commands and starting with a Service Principle that can create everything else. (#4)

--- destroy-environment.ps1
+++
@@ -1,8 +0,0 @@
-$InformationPreference = 'Continue' # Enable 'Write-Information' calls to show in the console.
-
-$ApplicationName = "Flexport Earth"
-
-Write-Information "Removing all resources from up your subscription..."
-Write-Information "Removing $ApplicationName Application..."
-
-Remove-AzADApplication -DisplayName $ApplicationName
