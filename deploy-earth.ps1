5cd701aa9b3a9c6b69de0da07673ab3b2fe2646f
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-04-14 12:39:04 -0700

    Azure pipeline testing (#8)
    * Adds azure-pipeline
    
    * Updating azure-pipeline.yml from online example for testing purposes.
    
    * Updates environment to match names in Azure DevOps.
    
    * Updates Azure subscription for test deployments.
    
    * Copy deployment script to the build artifacts directory.
    
    * Updates build info output dir.
    
    * Updating to run for PRs.
    
    * Fix Stage Name.
    
    * Update script path to execute deploy script.
    
    * Adjust slashes.
    
    * Copies the bicep file to artifacts and removes need to specify credentials to the script directly.
    
    * Fix path.
    
    * Trying to fix the script working dir.

--- deploy-earth.ps1
+++ deploy-earth.ps1
@@ -1,19 +1,19 @@
 # This file tested with azure-cli 2.33.1
-[CmdletBinding()]
-param (
-    [Parameter(Mandatory=$true)]
-    [String]
-    $ServicePrincipalUsername,
-    [Parameter(Mandatory=$true)]
-    [String]
-    $ServicePrincipalPassword,
-    [Parameter(Mandatory=$true)]
-    [String]
-    $ServicePrincipalTenant
-)
+# [CmdletBinding()]
+# param (
+#     [Parameter(Mandatory=$true)]
+#     [String]
+#     $ServicePrincipalUsername,
+#     [Parameter(Mandatory=$true)]
+#     [String]
+#     $ServicePrincipalPassword,
+#     [Parameter(Mandatory=$true)]
+#     [String]
+#     $ServicePrincipalTenant
+# )
 
 $ErrorActionPreference = "Stop"
 
-az login --service-principal -u $ServicePrincipalUsername -p $ServicePrincipalPassword --tenant $ServicePrincipalTenant
+# az login --service-principal -u $ServicePrincipalUsername -p $ServicePrincipalPassword --tenant $ServicePrincipalTenant
 
 az deployment sub create --location WestUS --template-file create-resource-group.bicep --parameters '{\"resourceGroupLocation\":{\"value\":\"WestUS\"}}'
