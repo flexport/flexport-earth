bc31ff7e49da970440efad65fffdabd92711e100
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-04-14 15:39:21 -0700

    Trying with slash escapes of doublequotes.

--- deploy-earth.ps1
+++ deploy-earth.ps1
@@ -20,4 +20,4 @@ az `
     deployment sub create `
     --location WestUS `
     --template-file create-resource-group.bicep `
-    --parameters '{\"resourceGroupLocation\":{\"value\":\"WestUS\"}}'
+    --parameters "{\"resourceGroupLocation\":{\"value\":\"WestUS\"}}"
