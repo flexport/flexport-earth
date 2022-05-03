bf755d78a7ae22f5ee00ead4631638271e312d22
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-05-02 18:45:51 -0700

    earth-53: Adds push script. (#49)

--- push.ps1
+++ push.ps1
@@ -1,1 +1,18 @@
-﻿Invoke-ScriptAnalyzer -Path **
+﻿Set-StrictMode –Version latest
+
+$ErrorActionPreference = "Stop"
+$InformationPreference = "Continue"
+
+# Run dependency management
+. ./releasables/dependencies/dependency-manager.ps1
+
+git fetch origin main
+
+$CurrentBranchName = git rev-parse --abbrev-ref HEAD
+$CommitsBehindOriginMain = git rev-list --count origin/main...$CurrentBranchName
+
+if ($CommitsBehindOriginMain -gt 0) {
+    Write-Error "The current branch is behind origin/main by $CommitsBehindOriginMain, please update it before continuing."
+}
+
+./build.ps1
