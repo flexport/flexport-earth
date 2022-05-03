b67209e58b965f7a4da30e807d700d617f4dbf9e
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-05-02 19:18:38 -0700

    earth-53: Fix tab.

--- push.ps1
+++ push.ps1
@@ -17,7 +17,7 @@ if (-Not ($GitStatus -like "*nothing to commit*")) {
 git fetch origin main
 
 $CurrentBranchName = git rev-parse --abbrev-ref HEAD
-$DiffCounts = (git rev-list --left-right --count origin/main...$CurrentBranchName).Split('\t')
+$DiffCounts = (git rev-list --left-right --count origin/main...$CurrentBranchName).Split('`t')
 $CommitsBehindOriginMain = $DiffCounts[0]
 
 if ($CommitsBehindOriginMain -gt 0) {
