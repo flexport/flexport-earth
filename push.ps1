9e6f0883be9b1293230298061f802dab68a684c1
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-05-02 19:07:06 -0700

    earth-53: Updates push script to check for pending changes and then runs all scripts before actually pushing.

--- push.ps1
+++ push.ps1
@@ -6,6 +6,14 @@ $InformationPreference = "Continue"
 # Run dependency management
 . ./releasables/dependencies/dependency-manager.ps1
 
+# Make sure there's no pending changes.
+$GitStatus = git status
+
+if (-Not ($GitStatus.Contains("nothing to commit"))) {
+    Write-Error "You have pending changes that need to be committed."
+}
+
+# Make sure we're testing with latest from origin/main.
 git fetch origin main
 
 $CurrentBranchName = git rev-parse --abbrev-ref HEAD
@@ -16,3 +24,7 @@ if ($CommitsBehindOriginMain -gt 0) {
 }
 
 ./build.ps1
+./deploy.ps1
+./destroy.ps1
+
+git push
