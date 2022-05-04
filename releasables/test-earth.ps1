fd503c2cc9cba71a0be515e8c6633748fc92a00e
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-05-04 08:52:59 -0700

    earth-53: Adds Cypress.io for functional testing.

---
+++ releasables/test-earth.ps1
@@ -0,0 +1,33 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [String]
+    $EnvironmentName,
+
+    [Parameter(Mandatory=$true)]
+    [String]
+    $EarthWebsiteDomainName
+)
+
+Set-StrictMode –Version latest
+
+$ErrorActionPreference = "Stop"
+$InformationPreference = "Continue"
+
+# Run dependency management
+. ./dependencies/dependency-manager.ps1
+
+# Load common configuration values
+. ./earth-config.ps1
+
+Write-Information ""
+Write-Information "Testing Earth in $EnvironmentName environment..."
+
+Write-Information ""
+Write-Information "Installing Cypress..."
+npm install cypress --save-dev
+Write-Information "Cypress installed!"
+Write-Information ""
+Write-Information "Running tests..."
+
+Invoke-Expression "$(npm bin)/cypress run --env EARTH_WEBSITE_URL=https://$EarthWebsiteDomainName"
