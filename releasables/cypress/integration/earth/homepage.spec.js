3932cf7a6e3d411948500fa429a235be0a4f1a56
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-05-04 10:06:36 -0700

    earth-53: Adds Cypress.io for functional testing. (#54)
    * earth-53: Adds Cypress.io for functional testing.
    
    * earth-53: Suppress PSAvoidUsingInvokeExpression warnings.
    
    * earth-53: Fix pipeline for pwsh task type.
    
    * earth-53: Restores inputs section to pipeline pwsh task.
    
    * earth-53: Trying to get the Powershell task to work...
    
    * earth-53: Add expected parameter to suppression tag.

---
+++ releasables/cypress/integration/earth/homepage.spec.js
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('Earth Homepage', () => {
+  beforeEach(() => {
+    cy.visit(Cypress.env('EARTH_WEBSITE_URL'))
+  })
+
+  it('displays the Homepage', () => {
+    // We use the `cy.get()` command to get all elements that match the selector.
+    // Then, we use `should` to assert that there are two matched items,
+    // which are the two default items.
+    cy.get('h1').should('contain', "Welcome to Flexport Earth")
+  })
+})
