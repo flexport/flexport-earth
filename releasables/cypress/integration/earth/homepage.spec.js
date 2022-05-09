5d068ead16aa5e2938e8ba26fc842935a6d539ff
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-05-09 12:24:16 -0700

    earth-46: Adds functional test to verify the Flexport image link.

--- releasables/cypress/integration/earth/homepage.spec.js
+++ releasables/cypress/integration/earth/homepage.spec.js
@@ -1,15 +1,6 @@
 /// <reference types="cypress" />
 
-// Welcome to Cypress!
-//
-// This spec file contains a variety of sample tests
-// for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
-// To learn more about how Cypress works and
-// what makes it such an awesome testing tool,
-// please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
+import HomePage from '../../flexport-earth-object-model/HomePage'
 
 describe('Earth Homepage', () => {
   beforeEach(() => {
@@ -17,9 +8,14 @@ describe('Earth Homepage', () => {
   })
 
   it('displays the Homepage', () => {
-    // We use the `cy.get()` command to get all elements that match the selector.
-    // Then, we use `should` to assert that there are two matched items,
-    // which are the two default items.
     cy.get('h1').should('contain', "Welcome to Flexport Earth")
+  })
+
+  it('links to flexport.com', () => {
+    new HomePage().Header.FlexportLogo.click()
+
+    cy
+      .url()
+      .should('eq', 'https://www.flexport.com/')
   })
 })
