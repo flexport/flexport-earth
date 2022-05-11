539c5cdc89361638622de0e86f19f0c7218467cc
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-05-11 10:59:52 -0700

    earth-63: Cleans up the test code a bit. (#70)
    * earth-63: Cleans up the test code a bit.
    
    * earth-63: Increase the deploy test attempts as sometimes it takes longer than 10x5sec.

--- releasables/cypress/integration/earth/homepage.spec.js
+++ releasables/cypress/integration/earth/homepage.spec.js
@@ -3,16 +3,26 @@
 import HomePage from '../../flexport-earth-object-model/HomePage'
 
 describe('Earth Homepage', () => {
+  let homePage;
+
   beforeEach(() => {
-    cy.visit(Cypress.env('EARTH_WEBSITE_URL'))
+    cy
+      .visit(Cypress.env('EARTH_WEBSITE_URL'))
+
+    homePage = new HomePage();
   })
 
   it('Displays the Homepage', () => {
-    cy.get('h1').should('contain', "Welcome to Earth");
+    homePage
+      .getBody()
+        .contains("Welcome to Earth");
   })
 
   it('Links to flexport.com', () => {
-    new HomePage().getHeader().FlexportLogo.click();
+    homePage
+      .getHeader()
+        .FlexportLogo
+          .click();
 
     cy
       .url()
@@ -20,16 +30,17 @@ describe('Earth Homepage', () => {
   })
 
   it('Shows correct build number', () => {
-    new HomePage().getFooter().BuildNumber
-    .then($els => {
-      // get Window reference from element
-      const win = $els[0].ownerDocument.defaultView
-      // use getComputedStyle to read the pseudo selector
-      const before = win.getComputedStyle($els[0], 'before')
-      // read the value of the `content` CSS property
-      const contentValue = before.getPropertyValue('content')
-      // the returned value will have double quotes around it, but this is correct
-      expect(contentValue).to.eq('"' + Cypress.env('BUILD_NUMBER') + '"')
-    })
+    homePage
+      .getFooter()
+        .BuildNumber.then($els => {
+          // get Window reference from element
+          const win = $els[0].ownerDocument.defaultView
+          // use getComputedStyle to read the pseudo selector
+          const before = win.getComputedStyle($els[0], 'before')
+          // read the value of the `content` CSS property
+          const contentValue = before.getPropertyValue('content')
+          // the returned value will have double quotes around it, but this is correct
+          expect(contentValue).to.eq('"' + Cypress.env('BUILD_NUMBER') + '"')
+        })
   })
 })
