1370fc9979d6736dcf9d682de011c7fd1b853e0c
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-06-03 10:50:05 -0700

    earth-88: Combined build scripts to simplify.

--- build.ps1
+++ build.ps1
@@ -1,4 +1,15 @@
-﻿Set-StrictMode –Version latest
+﻿[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $false)]
+    [String]
+    $BuildNumber,
+
+    [Parameter(Mandatory = $false)]
+    [String]
+    $BuildId
+)
+
+Set-StrictMode –Version latest
 
 $ErrorActionPreference = "Stop"
 $InformationPreference = "Continue"
@@ -6,15 +17,16 @@ $InformationPreference = "Continue"
 # Run dependency management
 . ./releasables/dependencies/dependency-manager.ps1
 
-$RunningNonInteractive = [Environment]::GetCommandLineArgs().Contains('-NonInteractive')
+# Load global development settings
+$GlobalDevelopmentSettings = Get-Content 'development-config.json' | ConvertFrom-Json
 
-if (-Not $RunningNonInteractive) {
-    . ./development-tools/local-config-manager.ps1
-}
+$DevelopmentToolsDirectory = $GlobalDevelopmentSettings.DevelopmentToolsDirectory
+$ReleasablesDirectory      = $GlobalDevelopmentSettings.ReleasablesDirectory
 
 # Install required tools
-./development-tools/install-development-tools.ps1
+$("$DevelopmentToolsDirectory/install-development-tools.ps1")
 
+# Validate all the PowerShell scripts
 $Results = Invoke-ScriptAnalyzer -Path **
 
 if ($Results) {
@@ -23,13 +35,46 @@ if ($Results) {
 }
 
 try {
-    Push-Location $ReleasablesPath
+    Push-Location $ReleasablesDirectory
 
     # Generate a random build number for local builds.
     $BuildNumber = [guid]::NewGuid()
 
-    ./build-earth.ps1 `
-        -BuildNumber $BuildNumber
+    Write-Information ""
+    Write-Information "Building Earth Website (Build Number: $BuildNumber | Build ID: $BuildId)"
+    Write-Information ""
+
+    Push-Location ./frontend/website-content/
+
+    $BuildNumberFilePath = "./styles/build-number.css"
+    "#build-number-anchor::before { content: ""$BuildNumber""; }" | Out-File -FilePath $BuildNumberFilePath
+    Write-Information "Build Number written to $BuildNumberFilePath"
+
+    # Update BuildID if available.
+    if ($BuildId) {
+        $IndexPath = "./pages/index.tsx"
+        $IndexContent = Get-Content -Path $IndexPath
+        $IndexContent = $IndexContent.Replace('{BUILDID}', $BuildId)
+        $IndexContent | Out-File -FilePath $IndexPath
+        Write-Information "Build ID written to $IndexPath"
+    }
+
+    Write-Information ""
+    Write-Information "Compiling website files..."
+    npm install
+    npm run build
+    Write-Information "Website files compiled successfully!"
+    Write-Information ""
+
+    Write-Information "Compressing website content..."
+    zip -ru website.zip ./ -x "website.zip"
+    Write-Information "Compression complete!"
+    Write-Information ""
+
+    Write-Information "Earth website build completed!"
+    Write-Information ""
+
+    Pop-Location
 }
 finally {
     Pop-Location
