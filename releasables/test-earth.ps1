5ad798e0b908a21dc8da2a1b2ca22d8d9f4f84a9
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-06-06 10:58:30 -0700

    earth-95: Manually restore symlinks.

--- releasables/test-earth.ps1
+++ releasables/test-earth.ps1
@@ -39,13 +39,38 @@ try {
         Remove-Item $TestResultsDirectory -Force -Recurse
     }
 
-    $CypressBinPath = "node_modules/cypress/bin/cypress"
+    $NpmBinPath = $(npm bin)
 
-    Write-Information "Getting cypress version"
-    & $CypressBinPath --version
+    ls -la $NpmBinPath
+
+    Write-Information "Restoring Symlinks..."
+    Write-Information "First, remove broken links from $NpmBinPath"
+    Push-Location $NpmBinPath
+    rm *.*
     Write-Information "Done!"
+    Write-Information "Recreate symlinks..."
+    ln -s ../cypress/bin/cypress cypress
+    ln -s ../extract-zip/cli.js extract-zip
+    ln -s ../is-ci/bin.js is-ci
+    ln -s ../which/bin/node-which node-which
+    ln -s ../rimraf/bin.js rimraf
+    ln -s ../semver/bin/semver.js semver
+    ln -s ../sshpk/bin/sshpk-conv sshpk-conv
+    ln -s ../sshpk/bin/sshpk-sign sshpk-sign
+    ln -s ../sshpk/bin/sshpk-verify sshpk-verify
+    ln -s ../uuid/dist/bin/uuid uuid
+    Pop-Location
+    Write-Information "Done!"
+    
+    Write-Information ""
+    ls -la $NpmBinPath
+    Write-Information ""
 
-    Invoke-Expression "$CypressBinPath run --spec ""cypress/integration/**/*"" --env BUILD_NUMBER=$BuildNumber,EARTH_WEBSITE_URL=$EarthWebsiteUrl --reporter junit --reporter-options ""mochaFile=results/cypress.xml"""
+    Write-Information "Node Version:    $(node --version)"
+    Write-Information "npm Version:     $(npm --version)"
+    Write-Information "cypress Version: $(& $NpmBinPath/cypress --version)"
+    
+    Invoke-Expression "$NpmBinPath/cypress run --spec ""cypress/integration/**/*"" --env BUILD_NUMBER=$BuildNumber,EARTH_WEBSITE_URL=$EarthWebsiteUrl --reporter junit --reporter-options ""mochaFile=results/cypress.xml"""
 
     if ($LastExitCode -ne 0) {
         Write-Error "Testing failed!"
