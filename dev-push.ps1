c7a9a95e3812f4b4a24c88a904fd5059eb46330e
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-06-11 13:33:26 -0700

    earth-95: Fix Push

--- dev-push.ps1
+++ dev-push.ps1
@@ -6,8 +6,12 @@ Set-StrictMode –Version latest
 $ErrorActionPreference = "Stop"
 $InformationPreference = "Continue"
 
+# Load global development settings
+$GlobalDevelopmentSettings = Get-Content 'development-config.json' | ConvertFrom-Json
+$ReleasablesPath           = $GlobalDevelopmentSettings.ReleasablesDirectory
+
 # Run dependency management
-. ./releasables/dependencies/dependency-manager.ps1
+. "$ReleasablesPath/dependencies/dependency-manager.ps1"
 
 # Make sure there's no pending changes.
 $GitStatus = git status
@@ -19,16 +23,16 @@ if (-Not ($GitStatus -like "*nothing to commit*")) {
 # Make sure we're testing with latest from origin/main.
 git fetch origin main
 
-$CurrentBranchName = git rev-parse --abbrev-ref HEAD
-$DiffCounts = ((git rev-list --left-right --count origin/main...$CurrentBranchName) -split '\t')
+$CurrentBranchName       = git rev-parse --abbrev-ref HEAD
+$DiffCounts              = ((git rev-list --left-right --count origin/main...$CurrentBranchName) -split '\t')
 $CommitsBehindOriginMain = $DiffCounts[0]
 
 if ($CommitsBehindOriginMain -gt 0) {
     Write-Error "The current branch is behind origin/main by $CommitsBehindOriginMain, please update it before continuing."
 }
 
-./build.ps1
-./deploy.ps1
-./destroy.ps1
+./dev-build.ps1
+./dev-deploy.ps1
+./dev-destroy.ps1
 
 git push --set-upstream origin $CurrentBranchName
