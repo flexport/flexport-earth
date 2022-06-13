b176a156d873265799a5a793742a38f373ac826d
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-06-13 08:52:33 -0700

    earth-35: Migrates pages to use NextJS Layouts for reusable layouts and reduce duplicate code.

--- src/website-content/pages/facts/countries/index.tsx
+++ src/website-content/pages/facts/countries/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import Link from 'next/link';
-import styles from '/styles/Home.module.css'
+import Layout from '../../../components/layout'
 
 export async function getStaticProps() {
   const countries = await (await fetch('https://restcountries.com/v3.1/all')).json();
@@ -25,26 +23,7 @@ type Countries = {
 
 const CountriesPage: NextPage<Countries> = ({countries}) => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Flexport Earth: Countries</title>
-        <meta name="description" content="Facts of global trade" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <header className={styles.pageHeader}>
-            <div className={styles.pageWikiTitle}>
-                <a id="flexport-logo" href="https://www.flexport.com"><Image src="/images/flexport-logo.svg" alt="Flexport Logo" height={20} width={90} /></a>
-                <span id="wiki-title-vertical-line" className={styles.wikiTitleVerticalLine}></span>
-                <span id="wiki-subtitle" className={styles.wikiSubTitle}>Wiki</span><br/>
-            </div>
-      </header>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Countries
-        </h1>
-
+    <Layout title='Countries' h1='Countries'>
         <ol>
           {countries.sort((a, b) => a.name.common.localeCompare(b.name.common)).map(({ name, cca2 }) => (
               <li key={name.common}>
@@ -52,23 +31,7 @@ const CountriesPage: NextPage<Countries> = ({countries}) => {
               </li>
             ))}
         </ol>
-      </main>
-
-      <footer className={styles.footer}>
-        <div className={styles.footerContent}>
-            <hr className={styles.footerContentHr}/>
-
-            <div>
-                <Image className={styles.footerContentFlexportLogo} layout="raw" src="/images/flexport-logo.svg" alt="Flexport Logo" height={20} width={90}/>
-                <span className={styles.footerContentTagLine}>We&apos;re making global trade easy for everyone.</span>
-            </div>
-
-            <a href="javascript:alert('Build URL not specified.');" className={styles.buildNumber}>
-                <div id="build-number-anchor"></div>
-            </a>
-        </div>
-      </footer>
-    </div>
+    </Layout>
   )
 }
 
