35db3ca73ac1755f550be3167c51caf4f4250f8f
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-06-14 11:58:29 -0700

    earth-35: Creates NextJs components out of the Layout, Header and Footer so they are easily reusable and removes duplicate code. (#99)

--- src/website-content/pages/index.tsx
+++ src/website-content/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/HomePage.module.css'
+import Footer from '../components/footer'
 
 const Home: NextPage = () => {
   return (
@@ -56,20 +57,7 @@ const Home: NextPage = () => {
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <div className={styles.footerContent}>
-            <hr className={styles.footerContentHr}/>
-
-            <div>
-                <Image className={styles.footerContentFlexportLogo} layout="raw" src="/images/flexport-logo.svg" alt="Flexport Logo" height={20} width={90}/>
-                <span className={styles.footerContentTagLine}>We&apos;re making global trade easy for everyone.</span>
-            </div>
-
-            <a href="javascript:alert('Build URL not specified.');" className={styles.buildNumber}>
-                <div id="build-number-anchor"></div>
-            </a>
-        </div>
-      </footer>
+      <Footer/>
     </div>
   )
 }
