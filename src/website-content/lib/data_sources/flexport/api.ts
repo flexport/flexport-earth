5a68591a36158ebfe8bb885590fa6033b680e002
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-06-15 10:45:35 -0700

    earth-35: Display all port info received from Flexport.

--- src/website-content/lib/data_sources/flexport/api.ts
+++ src/website-content/lib/data_sources/flexport/api.ts
@@ -126,8 +126,26 @@ export type Ports = {
 }
 
 export type Port = {
-    name:       string,
-    unlocode:   string
+    name:           string,
+    iata_code:      string,
+    icao_code:      string,
+    unlocode:       string,
+    cbp_port_code:  string
+    address: {
+        street_address:         string,
+        street_address2:        string,
+        locality:               string,
+        administrative_area:    string,
+        time_zone:              string,
+        country_code:           string,
+        geo_location: {
+            latitude:   string,
+            longitude:  string,
+            altitude:   string
+        },
+        postal_code:        string,
+        localized_address:  string
+    }
 }
 
 export default getFlexportApiClient;
