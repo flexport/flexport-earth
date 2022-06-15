961fb680894934caeae9a1571347e9e99d936fa5
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-06-15 11:33:16 -0700

    earth-35: Display all port info received from Flexport (#102)
    * earth-35: Display all port info received from Flexport.
    
    * earth-35: Convert Homepage a tags to NextJs Links.

--- src/website-content/pages/facts/places/port/[unlocode].tsx
+++ src/website-content/pages/facts/places/port/[unlocode].tsx
@@ -46,7 +46,33 @@ const CountryPage: NextPage<PortPageParams> = (params) => {
 
     return (
         <Layout title={params.port.name} h1={params.port.name}>
-
+            <ul>
+                <li>
+                    Address:
+                    <ul>
+                        <li>Street Address: {params.port.address.street_address}</li>
+                        <li>Street Address 2: {params.port.address.street_address2}</li>
+                        <li>Locality: {params.port.address.locality}</li>
+                        <li>Administrative Area: {params.port.address.administrative_area}</li>
+                        <li>Time Zone: {params.port.address.time_zone}</li>
+                        <li>Country Code: {params.port.address.country_code}</li>
+                        <li>Postal Code: {params.port.address.postal_code}</li>
+                        <li>Localized Address: {params.port.address.localized_address}</li>
+                        <li>
+                            Geo Location:
+                            <ul>
+                                <li>Latitude: {params.port.address.geo_location.latitude}</li>
+                                <li>Longitude: {params.port.address.geo_location.longitude}</li>
+                                <li>Altitude: {params.port.address.geo_location.altitude}</li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li>unlocode: {params.port.unlocode}</li>
+                <li>IATA Code: {params.port.iata_code}</li>
+                <li>CBP Port Code: {params.port.cbp_port_code}</li>
+                <li>ICAO Code: {params.port.icao_code}</li>
+            </ul>
         </Layout>
     )
 }
