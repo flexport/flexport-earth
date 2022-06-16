5b72c481a776fa20a6a16c6ddc68b484a9ab0cd7
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-06-15 18:55:24 -0700

    earth-113: Adds /facts/vehicles/vessels page and shows all vessels from Flexport API.

---
+++ src/website-content/pages/facts/vehicles/vessels/index.tsx
@@ -0,0 +1,45 @@
+import type { NextPage } from 'next'
+import { getFlexportApiClient, Vessel } from '../../../../lib/data_sources/flexport/api'
+import Layout from '../../../../components/layout'
+import Link from 'next/link';
+import Styles from '../../../../styles/facts/vehicles/vessels/index.module.css'
+
+export async function getStaticProps() {
+  const flexportApi   = await getFlexportApiClient();
+  const responseData  = await flexportApi.vehicles.getVessels()
+
+  console.log(responseData);
+
+  return {
+    props: {
+      time: new Date().toISOString(),
+      vessels: responseData.vessels.map(vessel => ({ name: vessel.name }))
+    },
+    revalidate: 3600
+  };
+}
+
+type Vessels = {
+  time: string,
+  vessels: [{
+    name: string
+  }]
+}
+
+const VesselsPage: NextPage<Vessels> = ({vessels, time}) => {
+  return (
+    <Layout title='Vessels' h1='Vessels'>
+        <ol>
+          {vessels.map(({ name }) => (
+              <li key={name} className={Styles.vessel}>
+                {name}
+              </li>
+            ))}
+        </ol>
+        <br/>
+        Data refreshed @ { time }
+    </Layout>
+  )
+}
+
+export default VesselsPage
