384ec416db5f6471805943018e4c5f381c7e3953
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-06-28 11:11:42 -0700

    earth-123: Removes NODE_TLS_REJECT_UNAUTHORIZED environment variable.

--- src/website-content/environment.d.ts
+++ src/website-content/environment.d.ts
@@ -2,6 +2,5 @@ declare namespace NodeJS {
     interface ProcessEnv {
       FLEXPORT_API_CLIENT_ID: string;
       FLEXPORT_API_CLIENT_SECRET: string;
-      NODE_TLS_REJECT_UNAUTHORIZED: string;
     }
 }
