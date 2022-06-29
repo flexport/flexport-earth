a2a7d46fe1ef9c5f79c481ed3833c0830e9b5582
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-06-29 11:26:34 -0700

    earth-147: Updates the footer to match new UX design.

--- src/website-content/components/footer.tsx
+++ src/website-content/components/footer.tsx
@@ -1,20 +1,79 @@
 import React, { ReactNode } from 'react';
-import styles from '../styles/Footer.module.css'
+import Styles from '../styles/Footer.module.css'
 import Image from 'next/image'
 
 const Footer = () => (
-      <footer className={styles.footer}>
-        <div className={styles.footerContent}>
-            <hr className={styles.footerContentHr}/>
-
-            <div>
-                <Image className={styles.footerContentFlexportLogo} layout="raw" src="/images/flexport-logo.svg" alt="Flexport Logo" height={20} width={90}/>
-                <span className={styles.footerContentTagLine}>We&apos;re making global trade easy for everyone.</span>
+      <footer className={Styles.footer}>
+        <div className={Styles.footerContent}>
+            <div className={Styles.footerMainContentSection}>
+                <div className={Styles.topPorts}>
+                    <div className={Styles.footerTopTitle}>TOP PORTS</div>
+                    <ol className={Styles.footerTopList}>
+                        <li>Ports name here</li>
+                        <li>Ports name here</li>
+                        <li>Ports name here</li>
+                        <li>Ports name here</li>
+                        <li>Ports name here</li>
+                        <li>Ports name here</li>
+                        <li>Ports name here</li>
+                        <li>Ports name here</li>
+                        <li>Ports name here</li>
+                        <li>Ports name here</li>
+                    </ol>
+                </div>
+                <div className={Styles.topVessels}>
+                    <div className={Styles.footerTopTitle}>TOP VESSELS</div>
+                    <ol className={Styles.footerTopList}>
+                        <li>Vessel name here</li>
+                        <li>Vessel name here</li>
+                        <li>Vessel name here</li>
+                        <li>Vessel name here</li>
+                        <li>Vessel name here</li>
+                        <li>Vessel name here</li>
+                        <li>Vessel name here</li>
+                        <li>Vessel name here</li>
+                        <li>Vessel name here</li>
+                        <li>Vessel name here</li>
+                    </ol>
+                </div>
+                <div className={Styles.glossaryAndInfo}>
+                    <div className={Styles.footerGlossary}>
+                        <div className={Styles.footerGlossaryLeft}>
+                            <div className={Styles.footerGlossaryTitle}>Glossary &gt;</div>
+                            <div className={Styles.footerGlossaryDescription}>Boost Your Supply Chain Terminology</div>
+                        </div>
+                        <div className={Styles.footerGlossaryRight}>
+                            <Image
+                                src="/images/icon-supply-chain-book.png"
+                                alt="Supply Chain Book"
+                                height={170}
+                                width={199}
+                            />
+                        </div>
+                    </div>
+                    <div className={Styles.footerInfo}>
+                        <div className={Styles.footerInfoTitle}>Interested in tech stack behind this website?</div>
+                        <div className={Styles.footerInfoDescription}>Read our public API documentation , or check out Github repository</div>
+                    </div>
+                </div>
             </div>
 
-            <a href="javascript:alert('Build URL not specified.');" className={styles.buildNumber}>
-                <div id="build-number-anchor"></div>
-            </a>
+            <hr className={Styles.footerContentHr}/>
+
+            <div className={Styles.footerLowerSection}>
+                <span className={Styles.footerLowerLeftSection}>
+                    <Image className={Styles.footerContentFlexportLogo} layout="raw" src="/images/flexport-logo.svg" alt="Flexport Logo" height={20} width={90}/>
+                    <div className={Styles.footerContentTagLine}>We&apos;re making global trade easy for everyone.</div>
+                </span>
+                <div className={Styles.footerLowerRightSection}>
+                    <a href="javascript:alert('Build URL not specified.');" className={Styles.buildNumber}>
+                        <div id="build-number-anchor"></div>
+                    </a>
+                    <div className={Styles.footerCopyright}>
+                        Copyright © 2022 Flexport, Inc.
+                    </div>
+                </div>
+            </div>
         </div>
       </footer>
 );
