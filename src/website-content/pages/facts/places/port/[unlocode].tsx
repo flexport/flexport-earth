b8330df240f72d21acca343ce3ab44cef5c20388
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-07-03 20:16:50 -0700

    earth-147: Styles the Port Details Page according to latest UX design. (#120)

--- src/website-content/pages/facts/places/port/[unlocode].tsx
+++ src/website-content/pages/facts/places/port/[unlocode].tsx
@@ -2,6 +2,9 @@ import type { NextPage } from 'next'
 import Layout from '../../../../components/layout'
 import { getFlexportApiClient, Port } from '../../../../lib/data_sources/flexport/api'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
+import Image from 'next/image'
+import Styles from '../../../../styles/facts/places/ports/index.module.css'
 
 type UNLoCodeParams = {
     params: {
@@ -35,7 +38,7 @@ export async function getStaticProps(params: UNLoCodeParams) {
     }
 }
 
-const CountryPage: NextPage<PortPageParams> = (params) => {
+const PortDetailPage: NextPage<PortPageParams> = (params) => {
     const router = useRouter();
 
     if (router.isFallback) {
@@ -45,38 +48,181 @@ const CountryPage: NextPage<PortPageParams> = (params) => {
     }
 
     return (
-        <Layout title={params.port.name}>
-            <h1>{params.port.name}</h1>
+        <Layout title={params.port.name} selectMajorLink='ports'>
+            <div className={Styles.breadcrumbs}>
+                <Link href='/'>Wiki</Link>&nbsp;&nbsp;&nbsp;
+                <Image
+                    src="/images/right-chevron.svg"
+                    alt="Right Chevron"
+                    height={10}
+                    width={10}
+                />
+                &nbsp;&nbsp;&nbsp;<Link href='/facts/places/ports'>Ports</Link>&nbsp;&nbsp;&nbsp;
+                <Image
+                    src="/images/right-chevron.svg"
+                    alt="Right Chevron"
+                    height={10}
+                    width={10}
+                />
+                &nbsp;&nbsp;&nbsp;<Link href={`/facts/places/ports/${params.port.address.country_code}`}>{params.port.address.country_code}</Link>&nbsp;&nbsp;&nbsp;
+                <Image
+                    src="/images/right-chevron.svg"
+                    alt="Right Chevron"
+                    height={10}
+                    width={10}
+                />
+                &nbsp;&nbsp;&nbsp;{params.port.name}&nbsp;&nbsp;&nbsp;
+            </div>
 
-            <ul>
-                <li>
-                    Address:
-                    <ul>
-                        <li>Street Address: {params.port.address.street_address}</li>
-                        <li>Street Address 2: {params.port.address.street_address2}</li>
-                        <li>Locality: {params.port.address.locality}</li>
-                        <li>Administrative Area: {params.port.address.administrative_area}</li>
-                        <li>Time Zone: {params.port.address.time_zone}</li>
-                        <li>Country Code: {params.port.address.country_code}</li>
-                        <li>Postal Code: {params.port.address.postal_code}</li>
-                        <li>Localized Address: {params.port.address.localized_address}</li>
-                        <li>
-                            Geo Location:
-                            <ul>
-                                <li>Latitude: {params.port.address.geo_location.latitude}</li>
-                                <li>Longitude: {params.port.address.geo_location.longitude}</li>
-                                <li>Altitude: {params.port.address.geo_location.altitude}</li>
-                            </ul>
-                        </li>
-                    </ul>
-                </li>
-                <li>unlocode: {params.port.unlocode}</li>
-                <li>IATA Code: {params.port.iata_code}</li>
-                <li>CBP Port Code: {params.port.cbp_port_code}</li>
-                <li>ICAO Code: {params.port.icao_code}</li>
-            </ul>
+            <div className={Styles.portDetailHeader}>
+                <div className={Styles.portDetailTitle}>
+                    <Image
+                        src={`https://assets.flexport.com/flags/svg/1/${params.port.address.country_code}.svg`}
+                        alt={`${params.port.address.country_code} Flag`}
+                        height={32}
+                        width={32}
+                    />
+
+                    <h1>{params.port.name} port</h1>
+                </div>
+            </div>
+
+            <div className={Styles.portDetail}>
+                <div className={Styles.portDetailLeft}>
+                    <div className={Styles.portDetailSubTitle}>About this port</div>
+
+                    <div className={Styles.portDetailSectionTitle}>
+                        General
+                    </div>
+
+                    <div className={Styles.portDetailField}>
+                        <div className={Styles.portDetailFieldName}>
+                            Port name
+                        </div>
+                        <div className={Styles.portDetailFieldValue}>
+                            {params.port.name}
+                        </div>
+                    </div>
+
+                    <div className={Styles.portDetailField}>
+                        <span className={Styles.portDetailFieldName}>
+                            Country
+                        </span>
+                        <span className={Styles.portDetailFieldValue}>
+                            {params.port.address.country_code}
+                        </span>
+                    </div>
+
+                    <div className={Styles.portDetailField}>
+                        <span className={Styles.portDetailFieldName}>
+                            Region/City
+                        </span>
+                        <span className={Styles.portDetailFieldValue}>
+                            {params.port.address.administrative_area}/{params.port.address.locality}
+                        </span>
+                    </div>
+
+                    <div className={Styles.portDetailField}>
+                        <span className={Styles.portDetailFieldName}>
+                            Address
+                        </span>
+                        <span className={Styles.portDetailFieldValue}>
+                            {params.port.address.street_address }
+                        </span>
+                    </div>
+
+                    <div className={Styles.portDetailField}>
+                        <span className={Styles.portDetailFieldName}>
+                            CBP Port code
+                        </span>
+                        <span className={Styles.portDetailFieldValue}>
+                            {params.port.cbp_port_code}
+                        </span>
+                    </div>
+
+                    <div className={Styles.portDetailField}>
+                        <span className={Styles.portDetailFieldName}>
+                            UNLoCode
+                        </span>
+                        <span className={Styles.portDetailFieldValue}>
+                            {params.port.unlocode}
+                        </span>
+                    </div>
+
+                    <div className={Styles.portDetailField}>
+                        <span className={Styles.portDetailFieldName}>
+                            IATA code
+                        </span>
+                        <span className={Styles.portDetailFieldValue}>
+                            {params.port.iata_code}
+                        </span>
+                    </div>
+
+                    <div className={Styles.portDetailField}>
+                        <span className={Styles.portDetailFieldName}>
+                            ICAO code
+                        </span>
+                        <span className={Styles.portDetailFieldValue}>
+                            {params.port.icao_code}
+                        </span>
+                    </div>
+                </div>
+
+                <div className={Styles.portDetailRight}>
+                    <div className={Styles.portDetailLocation}>
+                        <div className={Styles.portDetailSectionTitle}>
+                            Location
+                        </div>
+
+                        <div className={Styles.portDetailField}>
+                            <span className={Styles.portDetailFieldName}>
+                                Latitude
+                            </span>
+                            <span className={Styles.portDetailFieldValue}>
+                                {params.port.address.geo_location.latitude}
+                            </span>
+                        </div>
+
+                        <div className={Styles.portDetailField}>
+                            <span className={Styles.portDetailFieldName}>
+                                Longitude
+                            </span>
+                            <span className={Styles.portDetailFieldValue}>
+                                {params.port.address.geo_location.longitude}
+                            </span>
+                        </div>
+
+                        <div className={Styles.portDetailField}>
+                            <span className={Styles.portDetailFieldName}>
+                                <a
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    href='https://www.google.com/maps/search/?api=1&query=33.757314,-118.225765'>
+                                        View on map
+                                </a>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div className={Styles.pageDetailLearnMore}>
+                        <div className={Styles.portDetailSectionTitle}>
+                            Learn more about Flexport
+                        </div>
+                        <div className={Styles.portDetailField}>
+                            <span className={Styles.portDetailFieldName}>
+                                <a
+                                    target='_blank'
+                                    rel='noreferrer'
+                                    href='https://www.flexport.com'>
+                                        Visit website
+                                </a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
 
-export default CountryPage;
+export default PortDetailPage;
