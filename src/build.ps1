7977cafda92a2facdbc8cb427b2b6feaf2f6d1e9
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-08-05 15:09:08 -0700

    earth-178: Refactor build script into functions (#130)
    * earth-178: Refactors the build script into functions.

--- src/build.ps1
+++ src/build.ps1
@@ -22,10 +22,91 @@ Set-StrictMode –Version latest
 $ErrorActionPreference = "Stop"
 $InformationPreference = "Continue"
 
-$ScriptStartTime = Get-Date
+function Write-BuildNumber {
+    $BuildNumberFilePath = "./public/build-number.css"
 
-$ReleasablesDirectory = "releasables"
-$WebsiteContentSourceDirectory = "website-content"
+    "#build-number-anchor::before { content: ""$BuildNumber""; }" | Out-File -FilePath $BuildNumberFilePath
+
+    Write-Information "Build number written to $BuildNumberFilePath"
+}
+
+function Write-BuildUrl {
+    $IndexPath = "./pages/index.tsx"
+
+    $IndexContent = Get-Content -Path $IndexPath
+    $IndexContent = $IndexContent.Replace("javascript:alert('Build URL not specified.');", $BuildUrl)
+    $IndexContent | Out-File -FilePath $IndexPath
+
+    Write-Information "Build URL written to $IndexPath"
+}
+
+function Build-Website {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [String]
+        $FlexportApiClientId,
+
+        [Parameter(Mandatory = $true)]
+        [String]
+        $FlexportApiClientSecret
+    )
+
+    Write-Information ""
+    Write-Information "Compiling website files..."
+
+    npm install
+    if (!$?) {
+        Write-Error "Failed to install dependencies, see previous log entries."
+    }
+
+    $env:FLEXPORT_API_CLIENT_ID = "$FlexportApiClientId"; $env:FLEXPORT_API_CLIENT_SECRET = "$FlexportApiClientSecret"; npm run build
+    if (!$?) {
+        Write-Error "Failed to build the website, see previous log entries."
+    }
+
+    Write-Information "Website files compiled successfully!"
+    Write-Information ""
+}
+
+function Test-UnitAndComponentFunctionality {
+    Write-Information ""
+    Write-Information "Running unit tests..."
+
+    npm test
+
+    if (!$?) {
+        Write-Error "Unit tests failed to run successfully, see previous log entries."
+    }
+
+    Write-Information "Unit tests ran successfully!"
+    Write-Information ""
+}
+
+function Compress-Website {
+    $WebsiteContentOutputPath = "../$ReleasablesDirectory/frontend/content"
+
+    New-Item $WebsiteContentOutputPath `
+        -ItemType Directory `
+        -Force
+
+    $WebsiteContentZipOutputPath = "../$ReleasablesDirectory/frontend/content/website.zip"
+
+    Write-Information "Compressing website content to $WebsiteContentZipOutputPath"
+
+    zip -ru $WebsiteContentZipOutputPath ./ -x '.env'
+
+    if (!$?) {
+        Write-Error "Failed to compress the website, see previous log entries."
+    }
+
+    Write-Information "Compression complete!"
+    Write-Information ""
+}
+
+$ScriptStartTime                = Get-Date
+$ReleasablesDirectory           = "releasables"
+$WebsiteContentSourceDirectory  = "website-content"
 
 # Run dependency management
 . "$ReleasablesDirectory/dependencies/dependency-manager.ps1"
@@ -45,49 +126,21 @@ Write-Information ""
 try {
     Push-Location $WebsiteContentSourceDirectory
 
-    $BuildNumberFilePath = "./public/build-number.css"
-    "#build-number-anchor::before { content: ""$BuildNumber""; }" | Out-File -FilePath $BuildNumberFilePath
-    Write-Information "Build number written to $BuildNumberFilePath"
+    Write-BuildNumber
 
-    # Update BuildUrl if available.
+    # Update BuildUrl if available from CI system.
+    # Will not be available when running this script locally.
     if ($BuildUrl) {
-        $IndexPath = "./pages/index.tsx"
-        $IndexContent = Get-Content -Path $IndexPath
-        $IndexContent = $IndexContent.Replace("javascript:alert('Build URL not specified.');", $BuildUrl)
-        $IndexContent | Out-File -FilePath $IndexPath
-        Write-Information "Build URL written to $IndexPath"
+        Write-BuildUrl
     }
 
-    Write-Information ""
-    Write-Information "Compiling website files..."
-    npm install
-    $env:FLEXPORT_API_CLIENT_ID = "$FlexportApiClientId"; $env:FLEXPORT_API_CLIENT_SECRET = "$FlexportApiClientSecret"; npm run build
-    if (!$?) {
-        Write-Error "Failed to build the website, see previous log entries."
-    }
-    Write-Information "Website files compiled successfully!"
-    Write-Information ""
+    Build-Website `
+        -FlexportApiClientId     $FlexportApiClientId `
+        -FlexportApiClientSecret $FlexportApiClientSecret
 
-    Write-Information ""
-    Write-Information "Running unit tests..."
-    npm test
-    if (!$?) {
-        Write-Error "Unit tests failed to run successfully, see previous log entries."
-    }
-    Write-Information "Unit tests ran successfully!"
-    Write-Information ""
+    Test-UnitAndComponentFunctionality
 
-    $WebsiteContentOutputPath = "../$ReleasablesDirectory/frontend/content"
-    New-Item $WebsiteContentOutputPath -ItemType Directory -Force
-    $WebsiteContentZipOutputPath = "../$ReleasablesDirectory/frontend/content/website.zip"
-
-    Write-Information "Compressing website content to $WebsiteContentZipOutputPath"
-    zip -ru $WebsiteContentZipOutputPath ./ -x '.env'
-    if (!$?) {
-        Write-Error "Failed to compress the website, see previous log entries."
-    }
-    Write-Information "Compression complete!"
-    Write-Information ""
+    Compress-Website
 }
 finally {
     Pop-Location
