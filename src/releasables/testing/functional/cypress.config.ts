2959f55b299ff262f1314ec245bf2ff73c836b05
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-08-06 10:50:13 -0700

    earth-178: Convert cypress.config.js to Typescript.

--- src/releasables/testing/functional/cypress.config.ts
+++ src/releasables/testing/functional/cypress.config.ts
@@ -2,10 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    setupNodeEvents() {
       // implement node event listeners here
     },
     specPattern: "**/*.spec.js"
   },
   chromeWebSecurity: false
 });
+
+export {};
