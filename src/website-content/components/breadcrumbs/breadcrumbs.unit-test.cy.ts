51f3148d762c44b5833ad559fe572610897250ad
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-09-12 14:00:18 -0700

    earth-180: Adds `Titleize should replace hyphens with spaces` test.

--- src/website-content/components/breadcrumbs/breadcrumbs.unit-test.cy.ts
+++ src/website-content/components/breadcrumbs/breadcrumbs.unit-test.cy.ts
@@ -4,4 +4,8 @@ describe('Breadcrumbs', () => {
     it('Titleize should upper case first letter', () => {
         assert(titleize('test') == 'Test');
     })
+
+    it('Titleize should replace hyphens with spaces', () => {
+        assert(titleize('test-titleize') == 'Test Titleize');
+    })
 })
