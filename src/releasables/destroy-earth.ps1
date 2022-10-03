3ab65eb8e994f7fb0d04921b4e4776de6d88585d
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-10-03 08:16:48 -0700

    earth-208: Make it more clear that these configs are used at runtime.

--- src/releasables/destroy-earth.ps1
+++ src/releasables/destroy-earth.ps1
@@ -15,7 +15,7 @@ $InformationPreference = "Continue"
 
 if($PSCmdlet.ShouldProcess($EnvironmentName)) {
     # Load common configuration values
-    . ./earth-config.ps1
+    . ./earth-runtime-config.ps1
 
     Write-Information ""
     Write-Information "Destroying $EnvironmentName environment..."
