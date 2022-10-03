b001bff6b6b6ae01a081769fff2398abf9b36271
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-10-03 07:20:51 -0700

    earth-208: Wires up new models for the Port Detail Page to start exposing Terminals.

--- src/releasables/testing/functional/cypress/integration/earth/facts/places/port/PortPage.spec.ts
+++ src/releasables/testing/functional/cypress/integration/earth/facts/places/port/PortPage.spec.ts
@@ -5,24 +5,63 @@ import ListCountriesAndPortCountsPage from  '../../../../../flexport-earth-objec
 import PortsByCountryPage             from  '../../../../../flexport-earth-object-model/facts/places/ports/PortsByCountryPage'
 import PortPage                       from  '../../../../../flexport-earth-object-model/facts/places/port/PortPage'
 
+// TODO: EARTH-257: REFACTOR:
+//       Refactor test data into a central/reusable location.
+
+const CountryCodeUnitedStates     = 'US';
+const PortCanaveralName           = 'Port Canaveral';
+const PortCanaveralUnLoCode       = 'USPCV';
+const TerminalCanaveralChiwanName = 'CHIWAN CONTAINER TERMINAL (CCT)'
+
+const ValidCountryCode  = CountryCodeUnitedStates;
+const ValidCountryName  = PortCanaveralName;
+const ValidPortUnloCode = PortCanaveralUnLoCode;
+
 describe('Port', () => {
-  it('Can navigate to a port from the Homepage', () => {
+  it('Can navigate to a Valid Port from the Homepage', () => {
     gotoHomePage()
       .getAllPortsLink()
         .click();
 
       new ListCountriesAndPortCountsPage()
-        .getCountryPortsLink('US')
+        .getCountryPortsLink(ValidCountryCode)
           .click();
 
-      const sanDiegoPortUnLoCode = 'USPCV';
-
       new PortsByCountryPage()
-        .getPortLink(sanDiegoPortUnLoCode)
+        .getPortLink(ValidPortUnloCode)
           .click();
 
       new PortPage()
         .getBody()
-          .contains('Port Canaveral');
+          .contains(ValidCountryName);
+  })
+
+  it('Can view Terminals for a Valid Port', () => {
+    // Step 1: Go to Homepage
+    gotoHomePage()
+      .getAllPortsLink()
+        .click();
+
+    // TODO: EARTH-257: REFACTOR:
+    //       Refactor test page models to return subsequent models
+    //       instead of tests having to new them up.
+
+    // Step 2: Navigate to a Port that has Terminals
+    new ListCountriesAndPortCountsPage()
+    .getCountryPortsLink(ValidCountryCode)
+      .click();
+
+    new PortsByCountryPage()
+      .getPortLink(ValidPortUnloCode)
+        .click();
+
+    new PortPage()
+      .getBody()
+        .contains(ValidCountryName);
+
+    // Step 3: Validate that the Ports Terminals Appear as expected
+    new PortPage()
+      .getBody()
+        .contains(TerminalCanaveralChiwanName);
   })
 })
