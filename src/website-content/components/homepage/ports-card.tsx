4d95cc7e193bc3be539affd6616d1dbc8c4d1f6d
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-11-06 22:03:15 -0800

    earth-306: Fixes port number counts on the Homepage by pulling the values from the API instead of hard coding. Also starts breaking up the Homepage into sub components for testability and maintainability.

---
+++ src/website-content/components/homepage/ports-card.tsx
@@ -0,0 +1,63 @@
+import Image                        from 'next/image'
+import Link                         from 'next/link'
+
+import Styles from                  './ports-card.module.css'
+
+import PortIcon                     from '../../public/images/icon-port.svg'
+import RightChevron                 from '../../public/images/right-chevron.svg'
+
+export type PortsCardCountryInfo = {
+    countryName:      string,
+    cca2CountryCode:  string,
+    portCount:        number
+}
+
+const PortsCard = ({countries}: {countries: PortsCardCountryInfo[]}) => {
+
+    return (
+    <div className={Styles.portsSection}>
+        <a id="all-ports-link" href='facts/places/ports'>
+            <div className={Styles.portsLink}>
+            <Image
+                src={PortIcon}
+                alt="Port Icon"
+                height={26}
+                width={26}
+            />
+
+            <span className={Styles.portsSectionTitle}>Ports</span>
+
+            <span className={Styles.rightChevron}>
+                <Image
+                src={RightChevron}
+                alt="Right Chevron"
+                height={14}
+                width={14}
+                />
+            </span>
+            </div>
+        </a>
+
+        <div className={Styles.portCountries}>
+            {
+                countries.map(({ countryName, cca2CountryCode,portCount }) => (
+                    <Link key={cca2CountryCode} href={`facts/places/ports/${cca2CountryCode}`}>
+                        <div className={Styles.portCountry}>
+                            <Image
+                                src={`https://assets.flexport.com/flags/svg/1/${cca2CountryCode}.svg`}
+                                alt={`${countryName} flag`}
+                                height={16}
+                                width={16}
+                            />
+
+                            <span className={Styles.portCountryText}>{countryName} ports ( {portCount} )</span>
+                        </div>
+                    </Link>
+                ))
+            }
+        </div>
+    </div>
+  );
+}
+
+export default PortsCard;
--- src/website-content/pages/index.tsx
+++ src/website-content/pages/index.tsx
@@ -1,200 +1,70 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import Styles from '../styles/HomePage.module.css'
-import Footer from '../components/footer/footer'
-import FlexportLogo from '../public/images/flexport-logo.svg'
-import PortIcon from '../public/images/icon-port.svg'
-import RightChevron from '../public/images/right-chevron.svg'
-import VesselIcon from '../public/images/icon-vessel.svg'
-import ContainersIcon from '../public/images/icon-containers.svg'
-import Background from '../public/images/boat-background-optimized.webp'
 
-const Home: NextPage = () => {
+import Homepage                   from '../components/homepage/homepage'
+
+import getRestCountriesApiClient  from '../lib/data-sources/restcountries.com/api'
+import { getFlexportApiClient }   from '../lib/data-sources/flexport/api'
+
+type CountryInfo = {
+  countryName:      string,
+  cca2CountryCode:  string,
+  portCount:        number
+}
+
+export async function getServerSideProps() {
+  const countriesApi = getRestCountriesApiClient();
+  const flexportApi  = await getFlexportApiClient();
+  const countries    = await countriesApi.countries.getAllCountriesAsMap();
+
+  const cca2CountryPortsToHighlight = [
+    'CN',
+    'US',
+    'SG',
+    'KR',
+    'MY',
+    'JP'
+  ]
+
+  let countryMap: CountryInfo[] = [];
+
+  for (let i = 0; i < cca2CountryPortsToHighlight.length; i++) {
+    let cca2CountryCode = cca2CountryPortsToHighlight[i];
+
+    const ports = (await flexportApi.places.getSeaportsByCca2(cca2CountryCode)).ports;
+
+    let country = countries.get(cca2CountryCode)
+
+    if (!country) {
+      throw `Country not found for country code '${cca2CountryCode}'.`
+    }
+
+    const countryPorts: CountryInfo = {
+      countryName:      country.name.common,
+      cca2CountryCode:  cca2CountryCode,
+      portCount:        ports.length
+    };
+
+      countryMap.push(
+        countryPorts
+      );
+  }
+
+  return {
+    props: {
+      countryPortsToHighlight: countryMap
+    }
+  };
+}
+
+type HomepageViewModel = {
+  countryPortsToHighlight: CountryInfo[]
+}
+
+const Home: NextPage<HomepageViewModel> = ({ countryPortsToHighlight}) => {
   return (
-    <div className={Styles.container}>
-      <Head>
-        <title>Flexport Earth</title>
-        <meta name="description" content="Facts of global trade" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={Styles.main}>
-        <div className={Styles.mainBackgroundContainer}>
-          <Image
-            src={Background}
-            alt="Boat Background"
-            objectFit='cover'
-            layout='fill'
-            className={Styles.mainBackground}
-            priority={true}
-          />
-        </div>
-        <div className={Styles.mainContent}>
-          <Image
-            src={FlexportLogo}
-            alt="Flexport Logo"
-            height={30}
-            width={110}
-          />
-
-          <span className={Styles.wikiTitleVerticalLine}></span>
-
-          <span className={Styles.wikiSubTitle}>Wiki</span>
-
-          <div className={Styles.tagLine}>
-            Discover the world of supply chain
-          </div>
-
-          <div className={Styles.portsSection}>
-            <a id="all-ports-link" href='facts/places/ports'>
-              <div className={Styles.portsLink}>
-                <Image
-                  src={PortIcon}
-                  alt="Port Icon"
-                  height={26}
-                  width={26}
-                />
-
-                <span className={Styles.portsSectionTitle}>Ports</span>
-
-                <span className={Styles.rightChevron}>
-                  <Image
-                    src={RightChevron}
-                    alt="Right Chevron"
-                    height={14}
-                    width={14}
-                  />
-                </span>
-              </div>
-            </a>
-
-            <div className={Styles.portCountries}>
-              <Link href='facts/places/ports/CN'>
-                <div className={Styles.portCountry}>
-                  <Image
-                    src="https://assets.flexport.com/flags/svg/1/CN.svg"
-                    alt="China flag"
-                    height={16}
-                    width={16}
-                  />
-
-                  <span className={Styles.portCountryText}>China ports ( 260 )</span>
-                </div>
-              </Link>
-
-              <Link href='facts/places/ports/US'>
-                <div className={Styles.portCountry}>
-                  <Image
-                    src="https://assets.flexport.com/flags/svg/1/US.svg"
-                    alt="United States flag"
-                    height={16}
-                    width={16}
-                  />
-
-                  <span className={Styles.portCountryText}>United States ports ( 64 )</span>
-                </div>
-              </Link>
-
-              <Link href='facts/places/ports/SG'>
-                <div className={Styles.portCountry}>
-                  <Image
-                    src="https://assets.flexport.com/flags/svg/1/SG.svg"
-                    alt="Singapore flag"
-                    height={16}
-                    width={16}
-                  />
-
-                  <span className={Styles.portCountryText}>Singapore ports ( 2 )</span>
-                </div>
-              </Link>
-
-              <Link href='facts/places/ports/KR'>
-                <div className={Styles.portCountry}>
-                  <Image
-                    src="https://assets.flexport.com/flags/svg/1/KR.svg"
-                    alt="South Korea flag"
-                    height={16}
-                    width={16}
-                  />
-
-                  <span className={Styles.portCountryText}>South Korea ports ( 43 )</span>
-                </div>
-              </Link>
-
-              <Link href='facts/places/ports/MY'>
-                <div className={Styles.portCountry}>
-                  <Image
-                    src="https://assets.flexport.com/flags/svg/1/MY.svg"
-                    alt="Malaysia flag"
-                    height={16}
-                    width={16}
-                  />
-
-                  <span className={Styles.portCountryText}>Malaysia ports ( 13 )</span>
-                </div>
-              </Link>
-
-              <Link href='facts/places/ports/JP'>
-                <div className={Styles.portCountry}>
-                  <Image
-                    src="https://assets.flexport.com/flags/svg/1/JP.svg"
-                    alt="Japan flag"
-                    height={16}
-                    width={16}
-                  />
-
-                  Japan ports ( 383 )
-                </div>
-              </Link>
-            </div>
-          </div>
-
-          <div className={Styles.vesselsAndContainersSection}>
-            <a id="all-vessels-link" href='facts/vehicles/vessels'>
-              <div className={Styles.vesselsSection}>
-                <Image
-                  src={VesselIcon}
-                  alt="Vessel Icon"
-                  height={26}
-                  width={26}
-                />
-
-                <span className={Styles.vesselsSectionTitle}>Vessels</span>
-
-                <Image
-                  src={RightChevron}
-                  alt="Right Chevron"
-                  height={14}
-                  width={14}
-                />
-              </div>
-            </a>
-
-            <div className={Styles.containersSection}>
-              <Image
-                src={ContainersIcon}
-                alt="Containers Icon"
-                height={26}
-                width={26}
-              />
-
-            <span className={Styles.containersSectionTitle}>Containers</span>
-
-              <Image
-                src={RightChevron}
-                alt="Right Chevron"
-                height={14}
-                width={14}
-              />
-            </div>
-          </div>
-        </div>
-      </main>
-
-      <Footer/>
-    </div>
+    <Homepage
+      countryPortsToHighlight={countryPortsToHighlight}
+    />
   )
 }
 
