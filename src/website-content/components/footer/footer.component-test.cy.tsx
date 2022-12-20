e9eb03cd3fbe3e368b02a3e02c18e70170e0562f
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-12-20 14:54:09 -0800

    earth-263: Fix the product feedback test to be a little more flexible in terms of the URLs and User Agents, as they change depending on where the test is being run.

--- src/website-content/components/footer/footer.component-test.cy.tsx
+++ src/website-content/components/footer/footer.component-test.cy.tsx
@@ -10,17 +10,17 @@ describe('Footer', () => {
             cy.stub(win, 'open').as('windowOpen')
         });
 
-        // Act
         cy.mount(footerComponent);
 
-        // Assert
+        // Act
         cy.get(`.${Styles.productFeedbackLink}`)
             .click();
 
+        // Assert
         cy.get('@windowOpen')
             .should(
-                'be.calledWith',
-                'mailto:earth-feedback@flexport.com?subject=Earth Feedback&body=%0D%0A%0D%0A%0D%0A%0D%0A------------------------------------------------------%0D%0ATechnical Details:%0D%0A-Current Webpage: http://localhost:8080/__cypress/iframes//Users/matthewthomas/git/flexport-earth/src/website-content/components/footer/footer.component-test.cy.tsx%0D%0A-User Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Cypress/12.0.2 Chrome/106.0.5249.51 Electron/21.0.0 Safari/537.36%0D%0A-Screen Size: 1792x1120'
+                'be.calledWithMatch',
+                /mailto:earth-feedback@flexport.com\?subject=Earth Feedback&body=.+Current Webpage: http:\/\/localhost:8080\/.+\/src\/website-content\/components\/footer\/footer.component-test.cy.tsx.+User Agent: Mozilla\/5.0.+Screen Size: 1792x1120/
             );
     })
 })
