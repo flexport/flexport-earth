b9f6e7677ff5f30ecf85747f389091d52df216a3
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-12-20 15:17:52 -0800

    earth-263: Make the resolution a bit more dynamic

--- src/website-content/components/footer/footer.component-test.cy.tsx
+++ src/website-content/components/footer/footer.component-test.cy.tsx
@@ -4,23 +4,20 @@ import Styles from './footer.module.css'
 describe('Footer', () => {
     it('Product feedback includes technical details', () => {
         // Arrange
-        const footerComponent = <Footer />;
-
         cy.window().then((win) => {
             cy.stub(win, 'open').as('windowOpen')
         });
 
-        cy.mount(footerComponent);
+        cy.mount(<Footer />);
 
         // Act
-        cy.get(`.${Styles.productFeedbackLink}`)
-            .click();
+        cy.get(`.${Styles.productFeedbackLink}`).click();
 
         // Assert
         cy.get('@windowOpen')
             .should(
                 'be.calledWithMatch',
-                /mailto:earth-feedback@flexport.com\?subject=Earth Feedback&body=.+Current Webpage: http:\/\/localhost:8080\/.+\/src\/website-content\/components\/footer\/footer.component-test.cy.tsx.+User Agent: Mozilla\/5.0.+Screen Size: 1792x1120/
+                /mailto:earth-feedback@flexport.com\?subject=Earth Feedback&body=.+Current Webpage: http:\/\/localhost:8080\/.+\/src\/website-content\/components\/footer\/footer.component-test.cy.tsx.+User Agent: Mozilla\/5.0.+Screen Size: \d+x\d+/
             );
     })
 })
