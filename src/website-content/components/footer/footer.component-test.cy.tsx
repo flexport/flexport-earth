71ad411b5f92426134737f3e9819b190f3fe3c13
Matthew Thomas <10239689+matthew-thomas@users.noreply.github.com>
2022-12-20 10:14:36 -0800

    earth-263: Implements test to verify contents of sending a product feedback email.

--- src/website-content/components/footer/footer.component-test.cy.tsx
+++ src/website-content/components/footer/footer.component-test.cy.tsx
@@ -6,10 +6,21 @@ describe('Footer', () => {
         // Arrange
         const footerComponent = <Footer />;
 
+        cy.window().then((win) => {
+            cy.stub(win, 'open').as('windowOpen')
+        });
+
         // Act
         cy.mount(footerComponent);
 
         // Assert
-        const link = cy.get(`.${Styles.productFeedbackLink}`);
+        cy.get(`.${Styles.productFeedbackLink}`)
+            .click();
+
+        cy.get('@windowOpen')
+            .should(
+                'be.calledWith',
+                'mailto:earth-feedback@flexport.com?subject=Earth Feedback&body=%0D%0A%0D%0A%0D%0A%0D%0A------------------------------------------------------%0D%0ATechnical Details:%0D%0A-Current Webpage: http://localhost:8080/__cypress/iframes//Users/matthewthomas/git/flexport-earth/src/website-content/components/footer/footer.component-test.cy.tsx%0D%0A-User Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Cypress/12.0.2 Chrome/106.0.5249.51 Electron/21.0.0 Safari/537.36%0D%0A-Screen Size: 1792x1120'
+            );
     })
 })
